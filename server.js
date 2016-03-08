db69445ce02ead21a7c15005d670d6c6ff9457c7
mcmo <cmcmordie@charter.net>
2016-03-08 11:34:43 +0000

    added fcc user stories

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'use strict'
 const Hapi = require('hapi')
+const mongo = require('mongodb').MongoClient
+
+// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+const uri = 'mongodb://furluser:furlpw@ds023458.mlab.com:23458/f-url';
 
 const server = new Hapi.Server()
 server.connection({
@@ -9,10 +13,72 @@ server.connection({
 
 server.route({
   method: 'GET',
-  path: '/',
+  path: '/new/{url*}',
   handler: function(request, reply){
-    reply('hapi happy')
+    let url = request.params.url
+
+    if (!validUrl(url)) return reply({
+      original_url: url,
+      error: 'incorrect url format'
+    })
+    
+    let host = request.headers.host
+    processUrl(url, host, reply)
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/{num}',
+  handler: function(request, reply){
+    let short = request.params.num
+    let host = request.headers.host
+    processNum(short, host, reply)
   }
 })
 
 server.start(() => console.log('Started'))
+
+function processNum(short, host, reply){
+  mongo.connect(uri, function(err, db){
+    if (err) throw err
+    var urls = db.collection('urls')
+    urls.findOne({ short_url: +short }, function(err, result){
+      if (err) throw err
+      reply.redirect(result.original_url)
+    })
+  })
+}
+
+function validUrl(url) {
+  // regex url formula: http://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149
+  let re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+  return re.test(url)
+}
+
+function processUrl(url, host, cb){
+  mongo.connect(uri, function(err, db){
+    if (err) throw err
+    var urls = db.collection('urls')
+    
+    urls.findOne({ original_url: url }, function(err, result){
+      if (err) throw err
+      if (result){
+        return cb({original_url: result.original_url, short_url: host + '/' + result.short_url})
+      }
+      // find max short_url
+      urls.find().sort({_id:-1}).limit(1).toArray(function(err, result){
+        if (err) throw err
+        let nextShort = result[0] ? +result[0].short_url + 1 : 1
+        urls.insert({
+          original_url: url,
+          short_url: nextShort
+        }, function(err, result){
+          if (err) throw err
+          let ops = result.ops[0]
+          cb({original_url: ops.original_url, short_url: host + '/' + ops.short_url})
+        })
+      })
+    })
+  })
+}
